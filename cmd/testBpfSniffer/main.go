0cb163fe9d17907db7dad284865634c885d613e1
David Stainton <dstainton415@gmail.com>
2015-06-17 01:16:45 -0700

    Use our new sniffer changes after code review...

--- cmd/testBpfSniffer/main.go
+++ cmd/testBpfSniffer/main.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"fmt"
-	"github.com/david415/HoneyBadger/bpf_sniffer"
+	"os"
+
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/bsdbpf"
 	"github.com/google/gopacket/layers"
 )
 
 func main() {
 	var err error
-	sniffer := bpf_sniffer.NewBpfSniffer()
-	err = sniffer.Init("vio0")
+	sniffer := bsdbpf.NewBPFSniffer(os.Args[1], "", 32767, 3)
+	err = sniffer.Init()
 	if err != nil {
 		panic(err)
 	}
-
 	for {
-		timedFrame := sniffer.ReadTimedFrame()
+		frame, captureInfo, err := sniffer.ReadPacketData()
+		if err != nil {
+			panic(err)
+		}
 		// Decode a packet
-		fmt.Printf("timedFrame timestamp %s\n", timedFrame.Timestamp)
-		packet := gopacket.NewPacket(timedFrame.RawFrame, layers.LayerTypeEthernet, gopacket.Default)
+		fmt.Printf("frame timestamp %s\n", captureInfo.Timestamp)
+		packet := gopacket.NewPacket(frame, layers.LayerTypeEthernet, gopacket.Default)
 		// Get the TCP layer from this packet
 		if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
 			fmt.Println("This is a TCP packet!")
@@ -27,7 +31,6 @@ func main() {
 			tcp, _ := tcpLayer.(*layers.TCP)
 			fmt.Printf("From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
 		}
-
 		// Iterate over all layers, printing out each layer type
 		for _, layer := range packet.Layers() {
 			fmt.Println("PACKET LAYER:", layer.LayerType())
